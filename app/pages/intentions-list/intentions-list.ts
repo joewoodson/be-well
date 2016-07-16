03bf73ec73ad0fb2457280c7497bf92242e30040
Joe <joe@appstretto.com>
2016-07-16 14:18:48 +0200

    add intentions table to db

--- app/pages/intentions-list/intentions-list.ts
+++ app/pages/intentions-list/intentions-list.ts
@@ -1,23 +1,46 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { IntentionsService } from '../../providers/intentions-service/intentions-service';
+import { Intention, IntentionsService } from '../../providers/intentions-service/intentions-service';
 
 @Component({
   templateUrl: 'build/pages/intentions-list/intentions-list.html',
 })
 export class IntentionsListPage {
-	intentions: any;
+	intentions: Intention[];
 
   constructor(private nav: NavController, private intentionsService: IntentionsService) {
-  	this.intentionsService.getIntentions()
-    .then(intentions => {
-      this.intentions = intentions;
-      console.log(this.intentions);
-    });
+  	// this.intentionsService.getIntentions()
+   //  .then(intentions => {
+   //    this.intentions = intentions;
+   //    console.log(this.intentions);
+   //  });
   }
 
-  setIntention(){
-  	this.intentionsService.setIntention();
+  // setIntention(){
+  // 	this.intentionsService.setIntention();
+  // }
+
+  private loadIntentions(){
+    this.intentions = [];
+    this.intentionsService.getIntentions().then(
+      data => {
+        this.intentions = [];
+        if (data.res.rows.length > 0) {
+          for (var i = 0; i < data.res.rows.length; i++) {
+            let item = data.res.rows.item(i);
+            this.intentions.push(new Intention(item.id, item.text, item.active, item.freq));
+          }
+        }
+      });
+    // this.intentions = [
+    //   {text: 'test 1', active: false, freq: 3},
+    //   {text: 'test 2', active: true, freq: 6},
+    //   {text: 'test 3', active: true, freq: 1}      
+    // ]
+  }
+
+  private onPageDidEnter(){
+    this.loadIntentions();
   }
 
 }
--- app/providers/intentions-service/intentions-service.ts
+++ app/providers/intentions-service/intentions-service.ts
@@ -2,20 +2,35 @@ import { Injectable } from '@angular/core';
 import { Storage, SqlStorage } from 'ionic-angular';
 import { LocalNotifications } from 'ionic-native'
 
+export class Intention {
+  id: number;
+  text: string;
+  active: boolean;
+  freq: number;
+
+  constructor(id: number, text: string, active: boolean, freq: number){
+    this.id = id;
+    this.text = text;
+    this.active = active;
+    this.freq =freq;
+  }
+
+}
+
 @Injectable()
 export class IntentionsService {
   storage: Storage = null;
-  public alarms: any;
 
   constructor() {
-    this.storage = new Storage(SqlStorage);
+    this.storage = new Storage(SqlStorage, {name: 'be-wellDb'});
+    this.storage.query('CREATE TABLE IF NOT EXISTS intentions (id INTEGER PRIMARY KEY, text TEXT, active TEXT, frequency INTEGER)');
   }
 
-  getIntentions(){
-    return LocalNotifications.getAll();
+  public getIntentions(){
+    return this.storage.query('SELECT * FROM intentions');
   }
 
-  setIntention(){
+  public setIntention(){
     LocalNotifications.schedule({
       text: "Delayed Notification",
       at: new Date(new Date().getTime() + 5000),
