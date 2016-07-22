b090a5cfea87e878771b6ed3cc030cc932c72a4b
Joe <joe@appstretto.com>
2016-07-22 22:58:37 +0200

    add freq and active to detail page and save to db

--- app/providers/intentions-service/intentions-service.ts
+++ app/providers/intentions-service/intentions-service.ts
@@ -5,10 +5,10 @@ import { LocalNotifications } from 'ionic-native'
 export class Intention {
   id: number;
   text: string;
-  active: boolean;
+  active: number;
   freq: number;
 
-  constructor(id: number, text: string, active: boolean, freq: number){
+  constructor(id: number, text: string, active: number, freq: number){
     this.id = id;
     this.text = text;
     this.active = active;
@@ -23,26 +23,26 @@ export class IntentionsService {
 
   constructor() {
     this.storage = new Storage(SqlStorage, {name: 'be-wellDb'});
-    this.storage.query('CREATE TABLE IF NOT EXISTS intentions (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT, active TEXT, freq INTEGER)');
-    this.storage.query('INSERT INTO intentions (text, active, freq) VALUES (?,?,?)', ['this is a test intention', true, 3]);        
+    this.storage.query('CREATE TABLE IF NOT EXISTS intentions (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT, active INTEGER, freq INTEGER)');
+    this.storage.query('INSERT INTO intentions (text, active, freq) VALUES (?,?,?)', ['this is a test intention', 1, 3]);        
   }
 
   public getIntentions(){
     return this.storage.query('SELECT * FROM intentions');
     // return [
     //   {id: 1,text: 'test 1', active: false, freq: 3},
-    //   {id: 1,text: 'test 2', active: true, freq: 6},
-    //   {id: 1,text: 'test 3', active: true, freq: 1}      
+    //   {id: 1,text: 'test 2', active: 1, freq: 6},
+    //   {id: 1,text: 'test 3', active: 1, freq: 1}      
     // ]
   }
 
   public saveIntention(intention){
-    this.storage.query('INSERT INTO intentions (text, active, freq) VALUES (?,?,?)', [intention.text, true, 3]);    
+    this.storage.query('INSERT INTO intentions (text, active, freq) VALUES (?,?,?)', [intention.text, intention.active, intention.freq]);    
   }
 
   public updateIntention(intention){
     console.log(intention.id);
-    this.storage.query('UPDATE intentions SET text = \"' + intention.text + '\", active = \"' + true + '\", freq = \"' + 3 + '\" WHERE id = \"' + intention.id + '\"');    
+    this.storage.query('UPDATE intentions SET text = \"' + intention.text + '\", active = \"' + intention.active + '\", freq = \"' + intention.freq + '\" WHERE id = \"' + intention.id + '\"');    
   }
 
   public setIntention(){
@@ -55,7 +55,7 @@ export class IntentionsService {
   }
 
   ionViewLoaded(){
-    this.storage.query('INSERT INTO intentions (text, active, freq) VALUES (?,?,?)', ['another one', true, 3]);    
+    this.storage.query('INSERT INTO intentions (text, active, freq) VALUES (?,?,?)', ['another one', 1, 3]);    
   }
 
 }
